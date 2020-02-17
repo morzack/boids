a63c75edb4c4098ba792747bceb33374564baf0d
Patchkat <slingertv22@gmail.com>
2020-02-16 22:09:27 -0500

    Random Generation
    It is done
    It works
    It is good
    Boids spawn
    It chugs

--- Assets/Scripts/TreeGenerator.cs
+++ Assets/Scripts/TreeGenerator.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class place : MonoBehaviour
+public class TreeGenerator : MonoBehaviour
 {
     public GameObject tree1;
     public GameObject tree2;
@@ -11,12 +11,13 @@ public class place : MonoBehaviour
     public GameObject tree5;
     public float height;
     public Collider box;
+    public GameObject environment;
     public float numTrees;
     // Start is called before the first frame update
     void Start() {
         RaycastHit hit;
         GameObject tree;
-        Vector3 offset = new Vector3(0, 3, 0);
+        Vector3 offset = new Vector3(0, 1, 0);
         Bounds bounds = box.bounds;
         for (int x = 0; x < numTrees; x++){
             transform.position =  new Vector3(Random.Range(bounds.min.x, bounds.max.x),height,Random.Range(bounds.min.z, bounds.max.z));
@@ -24,18 +25,23 @@ public class place : MonoBehaviour
                 switch(Random.Range(1,6)){
                     case 1:
                         tree = Instantiate(tree1, hit.point + offset, Quaternion.identity);
+                        tree.transform.parent = environment.transform;
                         break;
                     case 2:
                         tree = Instantiate(tree2, hit.point + offset, Quaternion.identity);
+                        tree.transform.parent = environment.transform;
                         break;
                     case 3:
                         tree = Instantiate(tree3, hit.point + offset, Quaternion.identity);
+                        tree.transform.parent = environment.transform;
                         break;
                     case 4:
                         tree = Instantiate(tree4, hit.point + offset, Quaternion.identity);
+                        tree.transform.parent = environment.transform;
                         break;
                     case 5:
                         tree = Instantiate(tree5, hit.point + offset, Quaternion.identity);
+                        tree.transform.parent = environment.transform;
                         break;
                 }
                 
